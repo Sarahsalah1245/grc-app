74316c1af3e85c5decca12eaf62c074cddd172c5
Sarahsalah1245 <s-sarah.osman@zewailcity.edu.eg>
2026-07-04 23:18:07 +0300

    Update main.py

--- grc-app/grc-app/backend/app/main.py
+++ grc-app/grc-app/backend/app/main.py
@@ -18,6 +18,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[settings.FRONTEND_ORIGIN],
+    allow_origin_regex=r"https://grc-app.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
